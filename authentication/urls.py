ae570d712d3665a7c10f7085311180a572135cd7
Mujtaba Faizi <chaoticworld685@hotmail.com>
2019-07-15 21:20:32 +0500

    Issue Solved: Pass argument through the generic.DetailView to template

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('signup/save/', views.save, name='save'),
     path('signup/authenticate/', views.authenticate, name='authenticate'),
-    path('<int:pk>/user_homepage/', views.ShowUserHomepage.as_view(), name='user_home'),
+    path('<int:pk>/', views.ShowUserHomepage.as_view(), name='user_home'),
     path('signin/', views.SignIn.as_view(), name='signin'),
 
 ]
